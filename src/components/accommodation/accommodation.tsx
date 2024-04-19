a51352d7e533a1b9d13fbdf709088ac78f694800
Damian Fanaro <damianfanaro@gmail.com>
2024-04-20 01:51:26 +0200

    Add initial components

---
+++ src/components/accommodation/accommodation.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Accommodation = () => {
+    return <div>accommodation</div>;
+};
+
+export default Accommodation;
---
+++ src/components/journey/share-journey.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const shareJourney = () => {
+    return <div>shareJourney</div>;
+};
+
+export default shareJourney;
