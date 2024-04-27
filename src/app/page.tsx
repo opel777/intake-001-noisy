8e0483f5e52d55e2cd0c7287957a89439e68f180
delfina <delfiiduart@gmail.com>
2024-04-27 02:56:32 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
+import Landing from "@/components/landing/landing";
 
 export default function Home() {
   return (
+<<<<<<< HEAD
     <main>
       <h1>hola mundooo</h1>
     </main>
+=======
+        <Landing/>
+>>>>>>> cec3192 (Update)
   );
 }
--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -1,7 +1,24 @@
+'use client'
+import { Box, Stack, Text } from "@chakra-ui/react";
 import React from "react";
+import { shows } from "@/app/api/shows/data";
+import CarouselLanding from '@/components/landing/carousel/carousel';
 
-const Landing = () => {
-  return <div>landing</div>;
+const Landing : React.FC = () => {
+  return (
+    <Box textAlign="center" w='428px' h='540px'>
+      <Box display="inline-block" textAlign="left" ml='46px' mt={8}>
+        <Stack spacing={2}>
+          <Text fontSize='22px'>Todo lo que necesitas para vivir tu festival, en un solo lugar.</Text>
+          <Text fontSize='24px' color='#FF1086' as='b'>Are you ready?</Text> 
+        </Stack>
+      </Box>
+        
+      <Box ml='24px' mt='40px'>
+        <CarouselLanding cards={shows} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Landing;
